41f2aa1699a4683a3aea9aefcfebb3f2b5ea25b5
noelbn59 <noelbn59@gmail.com>
2025-08-19 11:42:51 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,102 @@
+# i = 1
+# for i in range(11):
+#     print(i)
+
+
+
+# i=1 
+# for i in range(20):  
+#   if i % 2 ==0:
+#     print(i)
+
+
+
+
+# i = 1
+# while i<= 20:
+#     if i % 2==0:
+#         print(i)
+#     i += 1
+
+
+
+# i = 1
+# for i in range(11):
+#     a = i * 2
+#     print(a)
+
+
+
+
+
+# i = 0
+# while i <= 10:
+#    i += 1
+#    if i == 5:
+#     break
+#    print(i)
+
+
+
+# i = 0
+# while i <= 9:
+#     i += 1
+#     if i == 7:
+#      continue
+#     print(i)
+
+
+
+# n=6
+# fact = 1
+# for i in range(1,n+1):
+#  fact *= i
+#  print(fact)
+
+
+
+
+# for i in range(1,21):
+#     if i % 3 == 0:
+#         continue
+#     print(i)
+
+
+
+
+# i = 0
+# while i <= 21:
+#     i += 1
+#     if i % 3==0:
+#         continue
+#     print(i)
+
+
+
+
+
+# i = 0
+# while i <= 99:
+#     i += 1
+#     if i % 12 ==0:
+#          break
+#     print(i)
+
+
+
+
+# for i in range(1,101):
+#     if 10 <= i <= 20:
+#         continue
+#     print(i)
+
+
+
+
+
+n = int(input("Enter a number to find factorial : "))
+fact = 1
+for i in range (1, n+1):
+    fact *= i
+    print(fact)
+
